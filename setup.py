6aacb02a5f5bc3bd23bad183345dbe04c9547795
Fabricio Castro <baptistafabricio87@gmail.com>
2023-10-27 09:42:41 -0300

    Configuracao para gerar bundle

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from os import path
-from setuptools import setup, find_packages
+from setuptools import find_packages  # setup
+from cx_Freeze import setup as cx_setup, Executable
+import sys
 
+base = None
+
+if sys.platform == "win32":
+    base = None
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
     readme = readme_file.read()
-
 
 with open(path.join(here, 'VERSION'), encoding='utf-8') as version_file:
     version = version_file.read()
@@ -16,14 +21,24 @@ with open(path.join(here, 'requirements.txt')) as requirements_file:
     requirements = [line for line in requirements_file.read().splitlines()
                     if not line.startswith('#')]
 
+executables = [Executable(script=r"bot_gn\bot.py", base=base)]
 
-setup(
+packages = ["idna"]
+options = {
+    "build_exe": {
+        "packages": packages,
+    },
+}
+
+cx_setup(
     name="bot_gn",
     version=version,
     description="Python package for a BotCity bot.",
     long_description=readme,
     long_description_content_type='text/markdown',
     packages=find_packages(exclude=['docs', 'tests']),
+    options=options,
+    executables=executables,
     include_package_data=True,
     package_data={
         "bot_gn": [
@@ -33,5 +48,25 @@ setup(
             'resources',
         ]
     },
-    install_requires=requirements,
+    # install_requires=requirements,
 )
+
+
+# setup(
+#     name="bot_gn",
+#     version=version,
+#     description="Python package for a BotCity bot.",
+#     long_description=readme,
+#     long_description_content_type='text/markdown',
+#     packages=find_packages(exclude=['docs', 'tests']),
+#     include_package_data=True,
+#     package_data={
+#         "bot_gn": [
+#             # When adding files here, remember to update MANIFEST.in as well,
+#             # or else they will not be included in the distribution on PyPI!
+#             # 'path/to/data_file',
+#             'resources',
+#         ]
+#     },
+#     install_requires=requirements,
+# )
